ef8be9665628b09b13f81a8d4363adf38a0ba42e
Matthew Will <mattwill09@gmail.com>
2013-03-15 22:15:21 -0500

    adding in knockout app

--- BasicKnockoutApp/MvcMovies/Migrations/Configuration.cs
+++ BasicKnockoutApp/MvcMovies/Migrations/Configuration.cs
@@ -21,7 +21,8 @@ namespace MvcMovies.Migrations
             Title = "When Harry Met Sally",
             ReleaseDate = DateTime.Parse("1989-1-11"),
             Genre = "Romantic Comedy",
-            Price = 7.99M, Rating = "G"
+            Price = 7.99M, 
+            Rating = "G"
         },
 
          new Movie
@@ -29,7 +30,8 @@ namespace MvcMovies.Migrations
              Title = "Ghostbusters ",
              ReleaseDate = DateTime.Parse("1984-3-13"),
              Genre = "Comedy",
-             Price = 8.99M
+             Price = 8.99M,
+             Rating = "PG"
          },
 
          new Movie
@@ -37,7 +39,8 @@ namespace MvcMovies.Migrations
              Title = "Ghostbusters 2",
              ReleaseDate = DateTime.Parse("1986-2-23"),
              Genre = "Comedy",
-             Price = 9.99M
+             Price = 9.99M,
+             Rating = "PG"
          },
 
        new Movie
@@ -45,7 +48,8 @@ namespace MvcMovies.Migrations
            Title = "Rio Bravo",
            ReleaseDate = DateTime.Parse("1959-4-15"),
            Genre = "Western",
-           Price = 3.99M
+           Price = 3.99M,
+           Rating = "G"
        }
        );
         }
